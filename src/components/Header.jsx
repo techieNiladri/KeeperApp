6fd72277b6593a70b4440130e4804e687d6bf361
techieNiladri <deyniladri937@gmail.com>
2025-11-25 16:12:38 +0530

    Initialize Keeper App

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import KeyboardCommandKeyIcon from "@mui/icons-material/KeyboardCommandKey";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <KeyboardCommandKeyIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
